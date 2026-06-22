bc922a5aaabd1b31c2441d52e600a8a99d08bdf5
cuenca87 <cuenca87@gmail.com>
2026-06-23 00:01:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = "emtusa-v1";
+self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(["/", "/index.html", "/icon.svg", "/manifest.json"])).then(() => self.skipWaiting())));
+self.addEventListener("activate", e => e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).then(r => { if (r.ok) { const c = r.clone(); caches.open(CACHE).then(cache => cache.put(e.request, c)); } return r; }).catch(() => caches.match(e.request)));
+});
